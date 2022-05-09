e76d4c704d2737b4f782ea7e2d23d01a5cd6d987
AmineFjr <74823891+AmineFajry@users.noreply.github.com>
2022-05-09 15:08:56 +0200

    ajout data microcontroller

--- front/src/components/ObjetConnecte/ObjetConnecte.vue
+++ front/src/components/ObjetConnecte/ObjetConnecte.vue
@@ -1,7 +1,9 @@
 <template>
-    <div>
-        Hello IOT
-    </div>
+  <v-data-table
+    :headers="headers"
+    :items="rows"
+    hide-default-footer
+  ></v-data-table>
 </template>
 
 <script>
@@ -10,13 +12,47 @@ export default {
     name:"ObjetConnecte",
     components:{},
     data(){
-        return{}
+        return{
+          headers: [
+          { text: 'Idantifient', value: 'id' },
+          { text: 'Adresse Mac', value: 'addrMac' },
+          { text: 'Seuil de Luminosité', value: 'seuilLuminosite'},
+          { text: 'Numéro User', value: 'user_id' },
+          { text: 'Crée', value: 'createdAt' },
+        ],
+        rows: [
+          {
+            id: null,
+            addrMac: '',
+            seuilLuminosite: '',
+            user_id: null,
+            createdAt: '',
+          }
+        ],
+        }
     },
-    mounted:function(){},
+    mounted:function(){
+        this.getIOTdata().then(response =>{
+            try{
+                let data = response.data
+                data.forEach(_data => {
+                    this.rows = [{
+                        id:_data.id,
+                        addrMac: _data.addrMac,
+                        seuilLuminosite: _data.seuilLuminosite,
+                        user_id: _data.user_id,
+                        createdAt: _data.createdAt
+                    }]
+                });
+          
+            } catch (error) {
+                 console.error("Parsing error:", error);
+            }
+        })
+    },
     methods:{
-     ...mapActions([]),
+     ...mapActions(['getIOTdata']),
     },
     computed:{}
-
 }
 </script>
